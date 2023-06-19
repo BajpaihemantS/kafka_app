4de8cdb7dbbea78d8b73973cee1c773705746b26
BajpaiHemantS <bajpaihemantsuresh@gmail.com>
2023-06-19 13:21:09 +0530

    build project third commi with producer and cosumer creator

--- src/main/java/com/springkafka/kafka_app/democontroller/KafkaRestController.java
+++ src/main/java/com/springkafka/kafka_app/democontroller/KafkaRestController.java
@@ -1,40 +1,47 @@
 package com.springkafka.kafka_app.democontroller;
 
-import com.springkafka.kafka_app.service.Producer;
+import com.springkafka.kafka_app.service.ConsumerRunner;
+import com.springkafka.kafka_app.service.ProducerRunner;
 import com.springkafka.kafka_app.utils.TopicEnum;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/producer")
 public class KafkaRestController {
 
-    Producer producer;
-    private String message;
-
-    @Autowired
-    public KafkaRestController(Producer producer) {
-        this.producer = producer;
-    }
-
-//    @GetMapping("/add-to-cart")
-//    public void getMessageForAddToCart(@RequestParam("message") String message){
-//        producer.sendMessage(TopicEnum.TOPIC1.getTopicName(),message);
-//    }
-
     @GetMapping("/add-to-cart")
-    public void getMessageForAddToCart(@RequestBody String message){
-        producer.sendMessage(TopicEnum.TOPIC1.getTopicName(),message);
+    public void getMessageForAddToCart(@RequestBody String message) throws Exception{
+        try {
+            ProducerRunner.runProducer(TopicEnum.TOPIC1.getTopicName(),message);
+        } catch (Exception e) {
+            System.out.println("Exception returned " + e);
+            e.printStackTrace();
+        }
     }
 
     @GetMapping("/buy-now")
     public void getMessageForBuyNow(@RequestBody String message){
-        producer.sendMessage(TopicEnum.TOPIC3.getTopicName(),message);
+        try {
+            ProducerRunner.runProducer(TopicEnum.TOPIC3.getTopicName(),message);
+        } catch (Exception e) {
+            System.out.println("Exception returned " + e);
+            e.printStackTrace();
+        }
     }
 
     @GetMapping("/add-to-wishlist")
-    public void getMessageForWishList(@RequestBody String message){
-        producer.sendMessage(TopicEnum.TOPIC2.getTopicName(),message);
+    public void getMessageForWishList(@RequestBody String message) {
+        try {
+            ProducerRunner.runProducer(TopicEnum.TOPIC2.getTopicName(), message);
+        } catch (Exception e) {
+            System.out.println("Exception returned " + e);
+            e.printStackTrace();
+        }
+    }
+
+    @GetMapping("/read-messages")
+    public void readProducedMessages(){
+        ConsumerRunner.runConsumer();
     }
 
 }
--- src/main/java/com/springkafka/kafka_app/service/Consumer2.java
+++ src/main/java/com/springkafka/kafka_app/service/Consumer2.java
@@ -6,11 +6,11 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class Consumer2 {
-    @KafkaListener(topics = "#{T(com.springkafka.kafka_app.utils.TopicEnum).getAllTopicNames()}", groupId = "#{T(com.springkafka.kafka_app.utils.GroupEnum).GROUP_1.getGroupName()}")
-    public void readMessage(ConsumerRecord<String, String> record){
-        long recordTime = record.timestamp();
-        long latency = System.currentTimeMillis() - recordTime;
-
-        System.out.println("The message recieved by 2nd consumer " + record.value() + " with latency " + latency);
-    }
+//    @KafkaListener(topics = "#{T(com.springkafka.kafka_app.utils.TopicEnum).getAllTopicNames()}", groupId = "#{T(com.springkafka.kafka_app.utils.GroupEnum).GROUP_1.getGroupName()}")
+//    public void readMessage(ConsumerRecord<String, String> record){
+//        long recordTime = record.timestamp();
+//        long latency = System.currentTimeMillis() - recordTime;
+//
+//        System.out.println("The message recieved by 2nd consumer " + record.value() + " with latency " + latency);
+//    }
 }
